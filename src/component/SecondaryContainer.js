5fb088207b293fe496fbbeba63fef3e017283ffa
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-09-18 23:57:45 +0530

    Viedo background Viedo title

--- src/component/SecondaryContainer.js
+++ src/component/SecondaryContainer.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import MovieList from './MovieList';
+import { useSelector } from 'react-redux';
 
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer</div>;
+  const movies=useSelector(store=>store.movies);
+  return (
+    <div>
+  <MovieList title={"Now Playing"} movies={movies.nowPlaying}/>
+  </div>
+  );
 
 };
 
