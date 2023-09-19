0974cc2cd958da77daae5c27dd6552820a9660c8
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-09-19 22:49:19 +0530

    gptsearchbar

--- src/component/GptSearchBar.js
+++ src/component/GptSearchBar.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import lang from '../utils/LanguageConstant';
 
 const GptSearchBar = () => {
+  const langKey=useSelector((store)=>store.config.slice);
   return (
     <div className="pt-[20%] flex justify-center">
     <form className="w-1/2 bg-black grid grid-cols-12">
-    <input type="text" className=" p-4 m-4 col-span-9" placeholder="what would you like watch today ?"/>
-    <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded lg">Search</button>
+    <input type="text" className=" p-4 m-4 col-span-9" placeholder={lang[langKey].gptSearchPlaceholder}/>
+    <button className="col-span-3 m-4 py-2 px-4 bg-red-700 text-white rounded lg"> {lang[langKey].search}</button>
     </form>
     </div>
   )
