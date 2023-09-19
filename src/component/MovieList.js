35c119a41ed621addca36e3ab54211c2864af8d7
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-09-19 12:59:53 +0530

    commit

--- src/component/MovieList.js
+++ src/component/MovieList.js
@@ -2,13 +2,13 @@ import React from 'react'
 import MovieCard from './MovieCard';
 
 const MovieList = ({title,movies}) => {
-    console.log(movies);
+    console.log(movies[0]);
   return (
     <div>
     <div>
     <h1>{title}</h1>
     <div>
-    <MovieCard posterPath={movies[0].poster_path} />
+    <MovieCard  />
     </div>
     </div>
   </div>
