c8899b9e30b582a1632a0f5676fcf7bcd726e434
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-09-23 22:52:53 +0530

    commit

--- src/component/MovieCard.js
+++ src/component/MovieCard.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IMG_CDN_URL } from '../utils/Constant';
 
 
 const MovieCard = ({posterPath}) => {
+  
+
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+ 
+  const imageStyle = {
+    transform: isHovered ? "scale(1.1)" : "scale(1)",
+    transition: "transform 0.5s ease-in-out",
+  };
   if(!posterPath) return null;
   return (
-  <div className="w-36 md:w-48 pr-4">
-  <img alt="Movie Card" src={IMG_CDN_URL+posterPath}/>
+  <div className="w-36 md:w-48 pr-4"onMouseEnter={handleMouseEnter}  onMouseLeave={handleMouseLeave}>
+  <img alt="Movie Card" src={IMG_CDN_URL+posterPath} style={imageStyle} />
   </div>
   );
   
