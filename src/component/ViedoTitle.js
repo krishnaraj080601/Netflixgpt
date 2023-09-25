3621c06bf7097625bb0bc2020c765ecc77f39563
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-09-25 19:56:37 +0530

    commit

--- src/component/ViedoTitle.js
+++ src/component/ViedoTitle.js
@@ -3,8 +3,8 @@ import React from 'react'
 const ViedoTitle = ({title,overview}) => {
   return ( 
     <div className="w-screen aspect-video pt-[20%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black">
-    <h1 className="text-2xl mt-16 md:mt-52 md:text-5xl md:mx-32 mx-8 font-bold">{title}</h1>
-    <p className="hidden md:inline-block my-16 mx-32 text-xl w-1/2 h-auto font-mono">{overview}</p>
+    <h1 className="text-2xl md:text-6xl font-bold">{title}</h1>
+    <p className="hidden md:inline-block py-6 text-lg w-1/4">{overview}</p>
     <div className="my-4 md:m-0">
     <button className=" bg-white text-black py-1 md:py-4 px-3 md:px-12 text-xl  rounded-lg hover:bg-opacity-80"> ▶ play</button>
     <button className="hidden md:inline-block mx-2  bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg">ℹ More Info</button>
