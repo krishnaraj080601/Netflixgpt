01612e3532f5a4c072b593e9d5616efd91a3d251
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-09-25 17:42:50 +0530

    commit

--- src/hooks/useTrendingMovies.js
+++ src/hooks/useTrendingMovies.js
@@ -8,7 +8,7 @@ const useTrendingMovies=()=>{
     const dispatch=useDispatch();
     const TrendingMovies = useSelector((store) => store.movies.popularMovies);
      const getTrendingMovies= async() =>{
-       const data=await fetch("https://api.themoviedb.org/3/trending/movie/day?language=en-US",Api);
+       const data=await fetch("https://api.themoviedb.org/3/movie/top_rated?page=1",Api);
      
        const json =await data.json();
        console.log(json.results);
